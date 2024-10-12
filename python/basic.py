729e9ea9f368a1fbb4b7ce252cbb01cdeb7d41a2
artist0pa <azambari2001@gmail.com>
2024-10-12 20:57:22 +0530

    1problem solved

---
+++ python/basic.py
@@ -0,0 +1,12 @@
+############ varaibles  ##############
+# in python evrything is a obje
+# we have mutable and unmutab data types 
+# when we asign a variable it assign a reference of 20 to num 
+
+# Problem 1: Swap two numbers without using a third variable
+
+num1=20
+num2=21 
+[num1,num2]=[num2,num1]  # this is the direct method used in python for swapping 
+
+print(num1)
