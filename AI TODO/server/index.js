cb70c8c090894cb5d5b24fb1777ad97dc03ced13
artist0pa <azambari2001@gmail.com>
2025-01-06 21:27:30 +0530

    todo app

---
+++ AI TODO/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require ('cors')
+const Dotenv = require('dotenv')
+Dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000
+app.use (express.json())
+app.get('/',(req,res)=>{
+        res.status(200).json("iam working")
+})
+app.listen(port, ()=>{
+        console.log(`the server is running on the port : ${port}`)
+})
+
+
+
